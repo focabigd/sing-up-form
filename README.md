706423163a1ce0d20b0588adc90a72d547b6eeb5
foca <123456789+minecraftxboxone16@gmail.com.github.com>
2024-07-01 10:35:23 +0300

    Add index.html

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # sing-up-form
+Photo taken by Halie West on Unsplash.Design by odin-project.
---
+++ javascript.js
@@ -0,0 +1,8 @@
+document.getElementById('sign-up-form').addEventListener('submit', function(event) {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+    if (password !== confirmPassword) {
+        alert('Passwords do not match.');
+        event.preventDefault();
+    }
+});
